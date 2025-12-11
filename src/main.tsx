42f963e996ebb6b180001d1d37848f2f4dbcb591
João Santos <dev.joaosantos@outlook.com>
2025-12-11 18:44:10 -0300

    feat: add  and  paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
 import "./style.css";
+import Home from "./Home.tsx";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
+const router = createBrowserRouter([
+  {
+    index: true,
+    Component: Home,
+  },
+  {
+    path: "app",
+    Component: App,
+  },
+]);
+
+const root = document.getElementById("root");
+
+createRoot(root!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
